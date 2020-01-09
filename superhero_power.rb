fa256230dca7a28be500141b1ec00a6e1a6fa5f6
Anurag <anurag.ganjiwale97@gmail.com>
2020-01-09 23:22:34 +0530

    [#170622055]Script of Superhero problem statement is added

---
+++ superhero_power.rb
@@ -0,0 +1,32 @@
+module Superpower
+  def check_possesed_power(power_name)
+
+    if(power_name == @power)
+      puts "Can #{power_name}"
+    else
+      puts "Cant #{power_name}"
+    end
+
+  end
+end
+
+class Superhero
+  include Superpower
+  def initialize(power)
+
+    @power=power
+
+  end
+end
+
+flash = Superhero.new("run")
+flash.check_possesed_power("run")
+
+spiderman= Superhero.new("fly")
+spiderman.check_possesed_power("fly")
+
+shaktiman = Superhero.new("run")
+shaktiman.check_possesed_power("jump")
+
+
+
